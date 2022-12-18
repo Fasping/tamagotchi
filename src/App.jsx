d3e42b7cf2b8d8de3b51330b4b507ca2ea051af9
Fernando Cases <nandocasesgarcia@gmail.com>
2022-12-19 00:03:54 +0100

    Clean up and build first comp

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Tamagotchi from './components/Tamagotchi'
+
+function App() {
+
+  return (
+    <div className="App">
+     <Tamagotchi />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Tamagotchi.jsx
@@ -0,0 +1,9 @@
+
+
+
+function Tamagotchi() {
+    return <h1>Tamagotchi component</h1>;
+    
+}
+
+export default Tamagotchi
